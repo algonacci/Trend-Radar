1194b85bbcf9b4632f7f094d9bb33b1678d4ca69
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2025-04-06 19:06:04 +0700

    specialized topics in news

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from youtube import get_youtube_trends
-from google_news import get_google_top_news
+from google_news import get_google_top_news, get_topic_news
 from google_trends import get_google_trends_indonesia
 from yahoo_finance import get_asia_and_indonesia_stock_exchange_info
 
@@ -11,12 +11,14 @@ app = Flask(__name__)
 def index():
     youtube_trends = get_youtube_trends()
     google_top_news = get_google_top_news()
+    topic_news = get_topic_news()
     google_trends = get_google_trends_indonesia()
     asia_markets, ihsg_data = get_asia_and_indonesia_stock_exchange_info()
     return render_template(
         "pages/index.html",
         youtube_trends=youtube_trends,
         google_top_news=google_top_news,
+        topic_news=topic_news,
         google_trends=google_trends,
         asia_markets=asia_markets,
         ihsg_data=ihsg_data
--- google_news.py
+++ google_news.py
@@ -20,3 +20,42 @@ def get_google_top_news():
         }
         results.append(formatted_article)
     return results
+
+
+def get_news_by_topic(topic):
+    # Create a new GNews instance with 10 results max
+    topic_gn = gnews.GNews(
+        language="id",
+        country="ID",
+        max_results=10,
+    )
+    
+    articles = topic_gn.get_news_by_topic(topic)
+    results = []
+    for article in articles:
+        formatted_article = {
+            "title": article.get("title", ""),
+            "url": article.get("url", ""),
+            "publisher": article.get("publisher", {}).get("title", ""),
+            "published_date": article.get("published date", ""),
+            "description": article.get("description", ""),
+            "topic": topic
+        }
+        results.append(formatted_article)
+    return results
+
+
+def get_topic_news():
+    topics = {
+        "BUSINESS": "Business",
+        "TECHNOLOGY": "Technology", 
+        "SCIENCE": "Science", 
+        "SPORTS": "Sports", 
+        "POLITICS": "Politics", 
+    }
+    
+    topic_news = {}
+    for topic_key, topic_name in topics.items():
+        topic_news[topic_name] = get_news_by_topic(topic_key)
+    
+    return topic_news
