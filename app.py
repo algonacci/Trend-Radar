7123de263ed75e13d3d090f25f41ad7206d2d07c
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2025-04-06 18:57:51 +0700

    finance data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from youtube import get_youtube_trends
 from google_news import get_google_top_news
 from google_trends import get_google_trends_indonesia
+from yahoo_finance import get_asia_and_indonesia_stock_exchange_info
 
 app = Flask(__name__)
 
@@ -11,11 +12,14 @@ def index():
     youtube_trends = get_youtube_trends()
     google_top_news = get_google_top_news()
     google_trends = get_google_trends_indonesia()
+    asia_markets, ihsg_data = get_asia_and_indonesia_stock_exchange_info()
     return render_template(
         "pages/index.html",
         youtube_trends=youtube_trends,
         google_top_news=google_top_news,
         google_trends=google_trends,
+        asia_markets=asia_markets,
+        ihsg_data=ihsg_data
     )
 
 
---
+++ yahoo_finance.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+
+def get_asia_and_indonesia_stock_exchange_info():
+    market = yf.Market("ASIA")
+    asia_market_data = market.summary
+    ihsg = yf.Ticker("^JKSE")
+    ihsg_data = ihsg.info
+    return asia_market_data, ihsg_data
